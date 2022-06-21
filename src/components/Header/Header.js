d42e6a4038c07803f4f8c2c2fed2938ab9450d3e
rkrana78 <rashedul.karim7858@gmail.com>
2022-06-22 02:01:38 +0600

    heeade section completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import images from '../../assets/welcome.png'
+import spoon from '../../assets/spoon.svg'
+import './Header.css'
 
 const Header = () => {
     return (
         <div className='main-header' id='home'>
             <div className="header-info">
-                <h4>Chase the new flavour</h4>
+                <h5>Chase the new flavour</h5>
+                <img style={{width:'45px'}} src={spoon} alt="" />
                 <h1 className='main-title'>The Key To Fine Dining</h1>
                 <p>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
                 <button className='custom-btn' >Explore Menu</button>
